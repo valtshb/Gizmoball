dd0eec27dcd332d948b0010e7f14978036c0a890
Valts Blezurs <cpb16163@uni.strath.ac.uk>
2019-02-13 16:38:16 +0200

    Triangle can now rotate and hopefully returns correct circles and lines

--- src/Model/TriangleGizmo.java
+++ src/Model/TriangleGizmo.java
@@ -9,27 +9,59 @@ import java.util.List;
 
 public class TriangleGizmo implements Gizmo {
 
+    // the right angle corner in triangle
+    private enum Rotation {
+        UP_LEFT, UP_RIGHT, DOWN_LEFT, DOWN_RIGHT
+    }
+
     private int xpos;
     private int ypos;
+    private Rotation rotation;
     private Color colour;
-    public TriangleGizmo(int x, int y){
+
+    public TriangleGizmo(int x, int y) {
         this.xpos = x;
         this.ypos = y;
+        rotation = Rotation.UP_LEFT;
         colour = Color.blue;
     }
 
     @Override
     public List<Circle> getCircles() {
         List l = new ArrayList<>();
-        l.add(new Circle(xpos, ypos, 0));
-        l.add(new Circle(xpos + 1, ypos, 0));
-        l.add(new Circle(xpos, ypos + 1, 0));
+        if (rotation != Rotation.DOWN_RIGHT)
+            l.add(new Circle(xpos, ypos, 0));
+
+        if (rotation != Rotation.DOWN_LEFT)
+            l.add(new Circle(xpos + 1, ypos, 0));
+
+        if (rotation != Rotation.UP_RIGHT)
+            l.add(new Circle(xpos, ypos + 1, 0));
+
+        if (rotation != Rotation.UP_LEFT)
+            l.add(new Circle(xpos + 1, ypos + 1, 0));
+
         return l;
     }
 
     @Override
     public List<LineSegment> getLines() {
-        return null;
+        List l = new ArrayList();
+        if (rotation == Rotation.UP_LEFT || rotation == Rotation.UP_RIGHT)
+            l.add(new LineSegment(xpos, ypos, xpos + 1, ypos));
+        else
+            l.add(new LineSegment(xpos, ypos + 1, xpos + 1, ypos + 1));
+
+        if (rotation == Rotation.UP_LEFT || rotation == Rotation.DOWN_LEFT)
+            l.add(new LineSegment(xpos, ypos, xpos, ypos + 1));
+        else
+            l.add(new LineSegment(xpos + 1, ypos, xpos + 1, ypos + 1));
+
+        if(rotation == Rotation.UP_LEFT || rotation == Rotation.DOWN_RIGHT)
+            l.add(new LineSegment(xpos + 1, ypos, xpos, ypos + 1));
+        else
+            l.add(new LineSegment(xpos, ypos, xpos + 1, ypos + 1));
+        return l;
     }
 
     @Override
@@ -42,11 +74,22 @@ public class TriangleGizmo implements Gizmo {
 
     }
 
-    public int getXpos(){
+    public void rotate() {
+        if (rotation == Rotation.UP_LEFT)
+            rotation = Rotation.UP_RIGHT;
+        else if (rotation == Rotation.UP_RIGHT)
+            rotation = Rotation.DOWN_RIGHT;
+        else if (rotation == Rotation.DOWN_RIGHT)
+            rotation = Rotation.DOWN_LEFT;
+        else
+            rotation = Rotation.UP_LEFT;
+    }
+
+    public int getXpos() {
         return xpos;
     }
 
-    public int getYpos(){
+    public int getYpos() {
         return ypos;
     }
 
