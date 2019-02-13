49b360c787102801c033c5b59c1fb84af5d6d4b7
Valts Blezurs <cpb16163@uni.strath.ac.uk>
2019-02-13 17:23:55 +0200

    redid model, merged triangle versions

--- src/Model/TriangleGizmo.java
+++ src/Model/TriangleGizmo.java
@@ -9,42 +9,75 @@ import java.util.List;
 
 public class TriangleGizmo implements Gizmo {
 
+    // the right angle corner in triangle
+    private enum Rotation {
+        TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT
+    }
+
     private int xpos;
     private int ypos;
     private Color colour;
-    private String state;
+    private Rotation rotation;
     private String name;
-    int x2;
-    int x3;
-    int y2;
-    int y3;
 
-
-    public TriangleGizmo(String n, int x, int y, String s){
+    public TriangleGizmo(String n, int x, int y, String s) {
         this.xpos = x;
         this.ypos = y;
         colour = Color.blue;
-        state = s;
-        name = n;
-         x2 = x;
-         x3 = x+1;
-         y2 = y+1;
-         y3 = y;
 
+        switch (s) {
+            case "topleft":
+                rotation = Rotation.TOP_LEFT;
+                break;
+            case "topright":
+                rotation = Rotation.TOP_RIGHT;
+                break;
+            case "bottomleft":
+                rotation = Rotation.BOTTOM_LEFT;
+                break;
+            default:
+                rotation = Rotation.BOTTOM_RIGHT;
+        }
+
+        name = n;
     }
 
     @Override
     public List<Circle> getCircles() {
-        List l = new ArrayList<>();
-        l.add(new Circle(xpos, ypos, 0));
-        l.add(new Circle(xpos + 1, ypos, 0));
-        l.add(new Circle(xpos, ypos + 1, 0));
+        List<Circle> l = new ArrayList<>();
+        if (rotation != Rotation.BOTTOM_RIGHT)
+            l.add(new Circle(xpos, ypos, 0));
+
+        if (rotation != Rotation.BOTTOM_LEFT)
+            l.add(new Circle(xpos + 1, ypos, 0));
+
+        if (rotation != Rotation.TOP_RIGHT)
+            l.add(new Circle(xpos, ypos + 1, 0));
+
+        if (rotation != Rotation.TOP_LEFT)
+            l.add(new Circle(xpos + 1, ypos + 1, 0));
+
         return l;
     }
 
     @Override
     public List<LineSegment> getLines() {
-        return null;
+        List<LineSegment> l = new ArrayList<>();
+        if (rotation == Rotation.TOP_LEFT || rotation == Rotation.TOP_RIGHT)
+            l.add(new LineSegment(xpos, ypos, xpos + 1, ypos));
+        else
+            l.add(new LineSegment(xpos, ypos + 1, xpos + 1, ypos + 1));
+
+        if (rotation == Rotation.TOP_LEFT || rotation == Rotation.BOTTOM_LEFT)
+            l.add(new LineSegment(xpos, ypos, xpos, ypos + 1));
+        else
+            l.add(new LineSegment(xpos + 1, ypos, xpos + 1, ypos + 1));
+
+        if (rotation == Rotation.TOP_LEFT || rotation == Rotation.BOTTOM_RIGHT)
+            l.add(new LineSegment(xpos + 1, ypos, xpos, ypos + 1));
+        else
+            l.add(new LineSegment(xpos, ypos, xpos + 1, ypos + 1));
+        return l;
     }
 
     @Override
@@ -57,65 +90,97 @@ public class TriangleGizmo implements Gizmo {
 
     }
 
-    public int getX2(){
-        return x2;
-    }
-    public int getX3(){
-        return x3;
-    }
-    public int gety2(){
-        return y2;
-    }
-    public int gety3(){
-        return y3;
-    }
-    public int getXpos(){
+    public int getXpos() {
         return xpos;
     }
 
-    public int getYpos(){
+    public int getYpos() {
         return ypos;
+    }
+
+    // Left dot
+    public int getX2() {
+        switch (rotation) {
+            case TOP_RIGHT:
+                return xpos - 1;
+            case BOTTOM_LEFT:
+                return xpos + 1;
+            default:
+                return xpos;
+        }
+    }
+
+    public int gety2() {
+        switch (rotation) {
+            case TOP_LEFT:
+                return ypos - 1;
+            case BOTTOM_RIGHT:
+                return ypos + 1;
+            default:
+                return ypos;
+        }
+    }
+
+
+    // Right dot
+    public int getX3() {
+        switch (rotation) {
+            case TOP_LEFT:
+                return xpos + 1;
+            case BOTTOM_RIGHT:
+                return xpos - 1;
+            default:
+                return xpos;
+        }
+    }
+
+    public int gety3() {
+        switch (rotation) {
+            case TOP_RIGHT:
+                return ypos + 1;
+            case BOTTOM_LEFT:
+                return ypos - 1;
+            default:
+                return ypos;
+        }
     }
 
     public Color getColour() {
         return colour;
     }
 
-    public void rotate(){
-        if(state.equals("topleft")){
-            System.out.println("doing this");
-
-            xpos = xpos + 1;
-            y2 = y2-1;
-            y3 = y3+1;
-            state = "topright";
-        }else if(state.equals("topright")){
-            ypos = ypos + 1;
-            x2++;
-            x3--;
-            state = "bottomright";
-        }else if(state.equals("bottomright")){
-            xpos = xpos -1;
-            y2++;
-            y3--;
-            state = "bottomleft";
-        }else if(state.equals("bottomleft")){
-            ypos = ypos -1;
-            x2--;
-            x3++;
-            state = "topleft";
+    public void rotate() {
+        switch (rotation) {
+            case TOP_LEFT:
+                rotation = Rotation.TOP_RIGHT;
+                break;
+            case TOP_RIGHT:
+                rotation = Rotation.BOTTOM_RIGHT;
+                break;
+            case BOTTOM_RIGHT:
+                rotation = Rotation.BOTTOM_LEFT;
+                break;
+            default:
+                rotation = Rotation.TOP_LEFT;
         }
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public String getState(){
-        return state;
+    public String getState() {
+        switch (rotation) {
+            case TOP_LEFT:
+                return "topleft";
+            case TOP_RIGHT:
+                return "topright";
+            case BOTTOM_LEFT:
+                return "bottomleft";
+            default:
+                return "bottomright";
+        }
     }
-
-
 
 }
 
