d660b2bf987128ecd500a1a1e705f37b5ee4ef53
alistairbrash <fjb16163@uni.strath.ac.uk>
2019-02-18 13:45:27 +0000

    keypress

---
+++ src/Controller/KeyPressedController.java
@@ -0,0 +1,49 @@
+package Controller;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import Model.AbsorberGizmo;
+
+public class KeyPressedController implements KeyListener {
+    String job;
+    public void keyTyped(KeyEvent e) {
+        displayInfo(e, "KEY TYPED: ");
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        System.out.println("got here");
+        int key = e.getKeyCode();
+       if(key == KeyEvent.VK_LEFT){
+            job = "fire";
+        }
+
+    }
+
+    public String getJob(){
+        return job;
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    private void displayInfo(KeyEvent e, String keyStatus) {
+
+        //You should only rely on the key char if the event
+        //is a key typed event.
+        int id = e.getID();
+        String keyString;
+        if (id == KeyEvent.KEY_TYPED) {
+            char c = e.getKeyChar();
+            keyString = "key character = '" + c + "'";
+        } else {
+            int keyCode = e.getKeyCode();
+            keyString = "key code = " + keyCode
+                    + " ("
+                    + KeyEvent.getKeyText(keyCode)
+                    + ")";
+        }
+    }
+}
