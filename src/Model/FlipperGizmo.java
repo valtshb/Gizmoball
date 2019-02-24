8f44817d684965c90ace51eebe88c886ac1764b8
Valts Blezurs <cpb16163@uni.strath.ac.uk>
2019-02-24 15:20:40 +0200

    flipgizmo returns circles now

--- src/Model/FlipperGizmo.java
+++ src/Model/FlipperGizmo.java
@@ -26,7 +26,7 @@ public class FlipperGizmo implements IGizmo {
         xpos = x;
         ypos = y;
         left = isLeft;
-        if(left)
+        if (left)
             rotation = Rotation.TOP_LEFT;
         else
             rotation = Rotation.TOP_RIGHT;
@@ -34,7 +34,84 @@ public class FlipperGizmo implements IGizmo {
 
     @Override
     public List<Circle> getCircles() {
-        return new ArrayList<>();
+        List<Circle> l = new ArrayList<>();
+        double x, y, x2, y2;
+        if (left) {
+            switch (rotation) {
+                case TOP_LEFT:
+                    x = xpos + .25;
+                    y = ypos + .25;
+                    x2 = x + 1.5 * Math.sin(Math.toRadians(90 - angle));
+                    y2 = y + 1.5 * Math.sin(Math.toRadians(angle));
+
+                    l.add(new Circle(x, y, .25));
+                    l.add(new Circle(x2, y2, .25));
+                    break;
+                case TOP_RIGHT:
+                    x = xpos + 1.75;
+                    y = ypos + .25;
+                    x2 = x - 1.5 * Math.sin(Math.toRadians(90 - angle));
+                    y2 = y + 1.5 * Math.sin(Math.toRadians(angle));
+
+                    l.add(new Circle(x, y, .25));
+                    l.add(new Circle(x2, y2, .25));
+                    break;
+                case BOTTOM_RIGHT:
+                    x = xpos + 1.75;
+                    y = ypos + 1.75;
+                    x2 = x - 1.5 * Math.sin(Math.toRadians(90 - angle));
+                    y2 = y - 1.5 * Math.sin(Math.toRadians(angle));
+
+                    l.add(new Circle(x, y, .25));
+                    l.add(new Circle(x2, y2, .25));
+                    break;
+                case BOTTOM_LEFT:
+                    x = xpos + .25;
+                    y = ypos + 1.75;
+                    x2 = x + 1.5 * Math.sin(Math.toRadians(90 - angle));
+                    y2 = y - 1.5 * Math.sin(Math.toRadians(angle));
+
+                    l.add(new Circle(x, y, .25));
+                    l.add(new Circle(x2, y2, .25));
+                    break;
+            }
+        } else {
+            switch (rotation) {
+                case TOP_RIGHT:
+                    x = xpos + 1.75;
+                    y = ypos + .25;
+                    x2 = x - 1.5 * Math.sin(Math.toRadians(90 - angle));
+                    y2 = y + 1.5 * Math.sin(Math.toRadians(angle));
+
+                    l.add(new Circle(x, y, .25));
+                    l.add(new Circle(x2, y2, .25));
+                case BOTTOM_RIGHT:
+                    x = xpos + 1.75;
+                    y = ypos + 1.75;
+                    x2 = x - 1.5 * Math.sin(Math.toRadians(90 - angle));
+                    y2 = y - 1.5 * Math.sin(Math.toRadians(angle));
+
+                    l.add(new Circle(x, y, .25));
+                    l.add(new Circle(x2, y2, .25));
+                case BOTTOM_LEFT:
+                    x = xpos + .25;
+                    y = ypos + 1.75;
+                    x2 = x + 1.5 * Math.sin(Math.toRadians(90 - angle));
+                    y2 = y - 1.5 * Math.sin(Math.toRadians(angle));
+
+                    l.add(new Circle(x, y, .25));
+                    l.add(new Circle(x2, y2, .25));
+                case TOP_LEFT:
+                    x = xpos + .25;
+                    y = ypos + .25;
+                    x2 = x + 1.5 * Math.sin(Math.toRadians(90 - angle));
+                    y2 = y + 1.5 * Math.sin(Math.toRadians(angle));
+
+                    l.add(new Circle(x, y, .25));
+                    l.add(new Circle(x2, y2, .25));
+            }
+        }
+        return l;
     }
 
     @Override
@@ -57,11 +134,11 @@ public class FlipperGizmo implements IGizmo {
         return id;
     }
 
-    public int getX(){
+    public int getX() {
         return xpos;
     }
 
-    public int getY(){
+    public int getY() {
         return ypos;
     }
 
@@ -69,11 +146,11 @@ public class FlipperGizmo implements IGizmo {
         return angle;
     }
 
-    public double getAngularVelocity(){
+    public double getAngularVelocity() {
         return angularVelocity;
     }
 
-    public void setAngle(double newAngle){
+    public void setAngle(double newAngle) {
         angle = newAngle;
     }
 
