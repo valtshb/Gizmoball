c72dc0c9c213d99f381f57623c4a4173c15bfc16
alistairbrash <fjb16163@uni.strath.ac.uk>
2019-03-19 16:08:58 +0000

    "absorb"er

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -93,7 +93,9 @@ public class Model extends Observable implements Cloneable {
                             gravity(ball, tuc);
 
                             if (cd.getGizmo() != null) {
-                                cd.getGizmo().trigger(ball);
+                                if(cd.getGizmo() instanceof AbsorberGizmo) {
+                                    cd.getGizmo().trigger(ball);
+                                }
                                 for (Connection connection : connections)
                                     if (connection.getTrigger() == cd.getGizmo())
                                         connection.triggered(ball);
